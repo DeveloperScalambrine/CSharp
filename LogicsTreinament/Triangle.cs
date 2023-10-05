4dda604c236b267f5e4f4b8cd9bcd3c20dcd45f0
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-10-05 02:08:17 -0300

    Altered method Escaleno

--- LogicsTreinament/Triangle.cs
+++ LogicsTreinament/Triangle.cs
@@ -1,10 +1,51 @@
 using System;
 
 namespace CSharp
-
 {
-    public class Triangle {
+    public class Triangle
+    {
+        public int SideA = 23;
+        public int SideB = 12;
+        public int SideC = 12;
+        bool message = true;
+        
+        public bool isTriangle()
+        {
+             
+            if (!(SideA < SideB + SideC) || (SideB < SideA + SideC) || (SideC < SideA + SideB)) 
+            {
+                message = false;
+            }           
+                return message;
+        }
 
+        public bool Equilatero(){
+            if ((SideA == SideB) && (SideB == SideC))
+            {
+                message = true;
+            
+            } else { message = false;}
+            
+            return message;
+        }
+
+        public bool Isosceles(){
+            if ((SideA == SideB) || (SideA == SideC) || (SideB == SideC)) 
+            {
+                message = true;
+            } else { message = false;}
+
+            return message;
+        }
+        
+        public bool Escaleno(){
+            if ((SideA != SideB) && (SideB != SideC) && (SideA != SideC)) 
+            {
+                message = true;
+            
+            } else { message = false;}
+            
+            return message;
+        }     
     }
- 
 }
--- Program.cs
+++ Program.cs
@@ -6,8 +6,41 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            Week dayWeek = new Week("sabado");
-            dayWeek.action();
+
+            Triangle triangle = new Triangle();
+            bool result = triangle.isTriangle();
+            bool resultEqui = triangle.Equilatero();
+            bool resultIso = triangle.Isosceles();
+            bool resultEsc = triangle.Escaleno();
+            if (result == false)
+            {
+                Console.WriteLine("Não é triângulo");
+            } else if (resultEqui == true)
+                {             
+                    Console.WriteLine("Equilátero");
+                } else if (resultIso == true)
+                  {
+                    Console.WriteLine("Isosceles");
+                  } else if (resultEsc == true)
+                  {
+                    Console.WriteLine("Escaleno");
+                  }
+                    
+            
+            // triangle.equilatero();
+            // triangle.isosceles();
+            // triangle.escaleno();
+            // Week dayWeek = new Week("sabado");
+            // dayWeek.action();
+            
+            // CountList countList = new CountList();
+            // countList.action();
+            // countList.count();
+
+            // AverageWithCondition.averageNote(AverageWithCondition.N1, AverageWithCondition.N2, AverageWithCondition.N3, AverageWithCondition.N4);
+   
+            // Week dayWeek = new Week("sabado");
+            // dayWeek.action();
             
             // CountList countList = new CountList();
             // countList.action();
