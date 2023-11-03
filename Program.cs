77ddcc7a6cb08f5ff16dfd6e7f3966a14a860b38
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-11-03 01:27:22 -0300

    done exercise two example using struct

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-               
+          Fracao f1 = new Fracao(5,2);
+          Fracao f2 = new Fracao(3,7);
+
+          Fracao f3 = f1.Multiplicar(f2);
+          Console.WriteLine(f3);            
+          Console.WriteLine(f3.CalculaFracao());
+
         }
     }
 }
