a1f8148b23153bca4e50600e4987883e5f2ef063
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-11-03 04:55:21 -0300

    examples with builders

--- SoftBlue/Objetos-Elementos-Estaticos/Examples.cs
+++ SoftBlue/Objetos-Elementos-Estaticos/Examples.cs
@@ -1,6 +1,38 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CSharp
 {
+  public class ObjEleStatics
+  {
+    public string firstChildren;
+    public string secondChildren;
+    public string thirdChildren;
+
+    public int AgeJ;
+    public int AgeP;
+    public int AgeS;
   
+    public ObjEleStatics() {
+      firstChildren = string.Empty;
+      secondChildren = string.Empty;
+      thirdChildren = string.Empty;
+    }
+
+    public ObjEleStatics(string nameJ, string nameP, string nameS) {
+      firstChildren = nameJ;
+      secondChildren = nameP;
+      thirdChildren = nameS;
+    }
+    public ObjEleStatics(int ageJ, int ageP, int ageS) : this("João","Pedro","Sarah"){
+      AgeJ = ageJ;
+      AgeP = ageP;
+      AgeS = ageS; 
+      
+    }
+        // public override string ToString()
+        // {
+        //     return $"Filho mais velho => é o {firstChildren} com {AgeJ} de idade: => {secondChildren} é o filho do meio com {AgeP} de idade: Por ultimo tem a Filha mais nova => {thirdChildren} com {AgeS} de idade";
+        // }
+    }
 }
