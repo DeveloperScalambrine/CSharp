41ab1b36ac9cb5bdbd79016d5c2a2d918bf7f033
Carlos Henrique <henrique.joaovictor@hotmail.com>
2023-12-14 07:56:50 -0300

    Organization code

--- Program.cs
+++ Program.cs
@@ -2,90 +2,11 @@ using System;
 
 namespace CSharp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-					TriangleOne triangleOne = new TriangleOne("jose");
-					// TriangleOne carlos = new TriangleOne("Carlos");
-					TriangleTwo.ExtensionMethod(triangleOne);
+	class Program
+	{
+		static void Main(string[] args)
+		{
 
-					// Console.WriteLine(carlos);
-					// TriangleTwo.ExtensionMethod(henrique);
-
-			//    Square square = new Square();
-			//    square.side = 2;
-			//    Console.WriteLine(square.CalcularArea());
-			// try
-			// {
-				
-			// 	// TriangleOne triangleOne = new TriangleOne();
-			// 	// Console.WriteLine(triangleOne.CalcularArea());
-			// 	TriangleOne.SetNumPaginas(-10);
-			// }
-			// catch (TriangleOneException e)
-			// {
-			// 	Console.WriteLine(e.Message);				
-			// }
-			// Circumference circumference = new Circumference();
-			// Console.WriteLine(circumference.CalcularArea());
-			// Trapeze trapeze = new Trapeze();
-			// trapeze.CalcularArea();
-			// Criação da turma
-		// 	Turma turma = new Turma();
-
-		// 	// Criação dos alunos
-		// 	Aluno aluno1 = new Aluno();
-		// 	Aluno aluno2 = new Aluno();
-		// 	Aluno aluno3 = new Aluno();
-
-		// 	// Associação entre os alunos e a turma
-		// 	turma.aluno1 = aluno1;
-		// 	turma.aluno2 = aluno2;
-		// 	turma.aluno3 = aluno3;
-
-		// 	// Cria as provas para cada aluno e define as notas
-
-		// 	Prova aluno1Prova1 = new Prova();
-		// 	aluno1Prova1.notaParte1 = 4.0;
-		// 	aluno1Prova1.notaParte2 = 2.5;
-		// 	aluno1.prova1 = aluno1Prova1;
-
-		// 	Prova aluno1Prova2 = new Prova();
-		// 	aluno1Prova2.notaParte1 = 1.0;
-		// 	aluno1Prova2.notaParte2 = 7.0;
-		// 	aluno1.prova2 = aluno1Prova2;
-
-		// 	Prova aluno2Prova1 = new Prova();
-		// 	aluno2Prova1.notaParte1 = 6.5;
-		// 	aluno2Prova1.notaParte2 = 3.5;
-		// 	aluno2.prova1 = aluno2Prova1;
-
-		// 	Prova aluno2Prova2 = new Prova();
-		// 	aluno2Prova2.notaParte1 = 0.0;
-		// 	aluno2Prova2.notaParte2 = 3.0;
-		// 	aluno2.prova2 = aluno2Prova2;
-
-		// 	Prova aluno3Prova1 = new Prova();
-		// 	aluno3Prova1.notaParte1 = 5.0;
-		// 	aluno3Prova1.notaParte2 = 4.0;
-		// 	aluno3.prova1 = aluno3Prova1;
-
-		// 	Prova aluno3Prova2 = new Prova();
-		// 	aluno3Prova2.notaParte1 = 6.0;
-		// 	aluno3Prova2.notaParte2 = 1.5;
-		// 	aluno3.prova2 = aluno3Prova2;
-
-		// 	// Imprime a média de cada aluno
-		// 	Console.WriteLine("Média Aluno 1: " + turma.aluno1.CalcularMedia());
-		// 	Console.WriteLine("Média Aluno 2: " + turma.aluno2.CalcularMedia());
-		// 	Console.WriteLine("Média Aluno 3: " + turma.aluno3.CalcularMedia());
-
-		// 	// Imprime a média da turma
-		// 	Console.WriteLine("Média Turma: " + turma.CalcularMedia());
-		// }
+		}
 	}
-}
-
- 
 }
